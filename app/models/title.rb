be602bf60429d809c3da7f34527abe3ee7d4406e
James Glover <jsaglover@gmail.com>
2024-05-20 11:36:21 +0100

    Add basic tests for title
    Just as with gender, we want basic tests for title.
    I've avoided using shared examples here as I find it a little premature for DRYing out.

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,2 +1,3 @@
 class Title < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
 end
--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Title, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build :title, name: name }
+
+  context 'when blank' do
+    let(:name) { "" }
+    it { is_expected.to be_invalid }
+  end
+
+  context 'when not unique' do
+    let(:name) { 'Mr' }
+    before { create :title, name: name }
+    it { is_expected.to be_invalid }
+  end
+
+  context 'when too long' do
+    let(:name) { 'Reverend'*30 }
+    it { is_expected.to be_invalid }
+  end
+
+  context 'when present and unique' do
+    let(:name) { 'Mr' }
+    before { create :title, name: 'Mrs' }
+    it { is_expected.to be_valid }
+  end
 end
