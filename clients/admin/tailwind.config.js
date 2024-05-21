ef34bc5f596448506a85719465fabec02349c96c
James Glover <jsaglover@gmail.com>
2024-05-21 13:19:45 +0100

    Add Tailwind accent colour
    By using a named accent colour we can
    maintain an consistent style

--- clients/admin/tailwind.config.js
+++ clients/admin/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from 'tailwindcss/colors'
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // We set a custom 'accent' colour which we can use sparingly to add a bit of
+      // variety. In this case we're just re-using the default 'purple' colour range
+      colors: { accent: colors.purple },
+    },
   },
   plugins: [],
 }
